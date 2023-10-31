b8a97895dc17707f4a355aec2a3872d5e1bdb92d
Akibur Rahman <me.akiburrahman@gmail.com>
2023-10-31 10:19:54 +0600

    .

--- app/lib/pages/login_page.dart
+++ app/lib/pages/login_page.dart
@@ -21,65 +21,67 @@ class _LoginPageState extends State<LoginPage> {
         child: Padding(
           padding: EdgeInsets.symmetric(horizontal: 25),
           child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(
-                  height: 50,
-                ),
-                //logo
-                Icon(
-                  Icons.message,
-                  size: 100,
-                ),
-                SizedBox(height: 50),
-                //welcome back message
-                Text(
-                  "Welcome back. You've been missed!",
-                  style: TextStyle(
-                    fontSize: 16,
-                    color: Colors.grey,
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    height: 50,
                   ),
-                ),
-
-                SizedBox(height: 25),
-
-                //emial textfield
-                MyTextField(
-                    controller: emailController,
-                    hintText: "email",
-                    obscureText: false),
-
-                SizedBox(height: 10),
-
-                //password textfield
-
-                MyTextField(
-                    controller: passwordController,
-                    hintText: "password",
-                    obscureText: true),
-                SizedBox(height: 25),
-
-                //login button
-                MyButton(onTap: () {}, text: "Sign In"),
-
-                //not a member mesasge
-                SizedBox(height: 50),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text("Not a member?"),
-                    SizedBox(width: 4),
-                    TextButton(
-                      onPressed: () {},
-                      child: Text(
-                        "Register Now",
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
+                  //logo
+                  Icon(
+                    Icons.message,
+                    size: 100,
+                  ),
+                  SizedBox(height: 50),
+                  //welcome back message
+                  Text(
+                    "Welcome back. You've been missed!",
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.grey,
                     ),
-                  ],
-                )
-              ],
+                  ),
+
+                  SizedBox(height: 25),
+
+                  //emial textfield
+                  MyTextField(
+                      controller: emailController,
+                      hintText: "email",
+                      obscureText: false),
+
+                  SizedBox(height: 10),
+
+                  //password textfield
+
+                  MyTextField(
+                      controller: passwordController,
+                      hintText: "password",
+                      obscureText: true),
+                  SizedBox(height: 25),
+
+                  //login button
+                  MyButton(onTap: () {}, text: "Sign In"),
+
+                  //not a member mesasge
+                  SizedBox(height: 50),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("Not a member?"),
+                      SizedBox(width: 4),
+                      TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          "Register Now",
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
             ),
           ),
         ),
