4ff119e9edb90224d2fa97a0a1d954db6992ba76
Akibur Rahman <me.akiburrahman@gmail.com>
2023-11-08 22:30:32 +0600

    Create chat_service.dart

---
+++ app/lib/services/chat/chat_service.dart
@@ -0,0 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class ChatService extends ChangeNotifier {
+  //get intense of auth and firestore
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  //senging message
+
+  //getting message
+}
