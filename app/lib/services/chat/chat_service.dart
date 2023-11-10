e20a10b9541774555a95a613e6c6d1f9d2b3eb5d
Akibur Rahman <me.akiburrahman@gmail.com>
2023-11-10 22:43:54 +0600

    .

--- app/lib/services/chat/chat_service.dart
+++ app/lib/services/chat/chat_service.dart
@@ -1,3 +1,4 @@
+import 'package:app/model/message.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -8,6 +9,25 @@ class ChatService extends ChangeNotifier {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   //senging message
+  Future<void> sendMessage(String reciverId, String message) async {
+    //get current user info'
+    final String currentUserID = _auth.currentUser!.uid;
+    final String currentUserEmail = _auth.currentUser!.email.toString();
+    final timestamp = Timestamp.now();
+
+    //create a new message
+    Message newMessage = Message(
+        senderId: currentUserID,
+        senderEmail: currentUserEmail,
+        recieverId: reciverId,
+        message: message,
+        timestamp: timestamp);
+    //construct chat room id for the current user and also for reciever id
+    List<String> ids = [currentUserID, reciverId];
+    ids.sort();
+    String chatRoomId = ids.join("_");
+    //add new message to database
+  }
 
   //getting message
 }
